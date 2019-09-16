4261351f4215b1072ced016de367475f3bee1d45
Juan Latorre <juanlatorre@protonmail.com>
2019-09-15 22:54:14 -0400

    hero done

--- packages/website/src/components/Hero.svelte
+++ packages/website/src/components/Hero.svelte
@@ -13,34 +13,26 @@
   h1 {
     padding: 0em;
     margin-bottom: 0em;
+    margin-top: -0.3em;
   }
 
   h2 {
     margin-top: 0em;
   }
 
-  hr {
-    width: 30vw;
-    height: 30px;
-    border-style: solid;
-    border-color: #f8efba;
-    border-width: 1px 0 0 0;
-  }
-  /* hr:before {
-    display: block;
-    content: "";
-    height: 30px;
-    margin-top: -31px;
-    border-style: solid;
-    border-color: #f8efba;
-    border-width: 0 0 1px 0;
-    border-radius: 20px;
-  } */
-
   img {
     animation: spin 150s infinite linear;
-    width: 40em;
-    margin-top: -20em;
+    width: 60em;
+    margin-top: -30em;
+    cursor: default;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  img::selection {
+    background: none;
   }
 
   @keyframes spin {
@@ -51,15 +43,36 @@
       transform: rotate(360deg);
     }
   }
+
+  .goDown {
+    font-size: 3rem;
+    text-decoration: none;
+    animation: mover 1s infinite alternate;
+    color: #f8efba;
+  }
+
+  .goDown:hover {
+    color: #eab543;
+  }
+
+  @keyframes mover {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-10px);
+    }
+  }
 </style>
 
 <div id="hero">
   <img src="img/mandala.svg" alt="Mandala" draggable="false" />
 
-  <h1 class="studioName is-size-1">Jaal Studio</h1>
-  <hr />
-  <!-- <h2>
+  <h1 class="is-size-1">Jaal Studio</h1>
+  <!-- <hr /> -->
+  <h2 class="is-size-3">
     Destaca tu negocio con un sitio web profesional, tienda online, aplicación
     móvil o portafolio.
-  </h2> -->
+  </h2>
+  <a href="#" class="goDown">⮟</a>
 </div>
